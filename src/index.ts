287f2c884cea0745cbc0a9c139f4752145dd9d91
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-11-04 12:14:15 -0300

    agregaPosicion

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ let num: number[] = new Array(7);
 for (let i: number = 0; i <= 6; i++) {
   let numero = Number(prompt("ingrese un num"));
   num[i] = numero;
-  console.log(num[i]);
+  console.log("el numero en la posicion", i, " es ", num[i]);
 }
